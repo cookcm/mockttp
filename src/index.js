c14a063a8b76149c9d8caa475c564af3adcaa493
Catherine Cook <cookcm@us.ibm.com>
2022-04-27 10:24:14 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+(async () => {
+  const mockttp = require("mockttp");
+
+  // Create a proxy server with a self-signed HTTPS CA certificate:
+  const https = await mockttp.generateCACertificate();
+  const server = mockttp.getLocal({ https });
+
+  // Inject 'Hello world' responses for all requests
+  await server.anyRequest().thenReply(200, "Hello world");
+  await server.start();
+
+  const caFingerprint = mockttp.generateSPKIFingerprint(https.cert);
+
+  if (process.argv[2] === "chrome") {
+    // Launch an intercepted Chrome using this proxy:
+    const launchChrome = require("./launch-chrome");
+    launchChrome("https://example.com", server, caFingerprint);
+  } else {
+    // Print out the server details for manual configuration:
+    console.log(`Server running on port ${server.port}`);
+    console.log(`CA cert fingerprint ${caFingerprint}`);
+  }
+})(); // (All run in an async wrapper, so we can easily use top-level await)
